210a772a60eb101de29848bbb72d9ea6990e1343
Gabriel John Solomon <153481400+Gabi-comm@users.noreply.github.com>
2026-06-16 08:47:07 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,22 +2,18 @@ from ultralytics import YOLO
 from roboflow import Roboflow
 
 if __name__ == '__main__':
-    # 1. Initialize YOLO model
     model = YOLO("yolov8n.pt")
-    
-    # 2. Download dataset from Roboflow
+
     rf = Roboflow(api_key="JVjvj0TbGk9Sr2npNrr0")
     project = rf.workspace("gabriel-zisua").project("gcash-receipt-detect-srujc")
     version = project.version(2)
     dataset = version.download("yolov8")
-                
-    # 3. Train the model using your GPU
+
     model.train(
         data=f"{dataset.location}/data.yaml",  
         epochs=100,                           
         imgsz=640,                            
         device=0,
-        workers=0  # <--- Crucial for Windows! Prevents recursive process spawning crashes.
+        workers=0 
     )
-
     print("done")
