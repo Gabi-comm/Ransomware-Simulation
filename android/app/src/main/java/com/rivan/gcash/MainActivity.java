9a57b21c9ee1404ae43972798a198821b1a84e07
Gabriel John Solomon <153481400+Gabi-comm@users.noreply.github.com>
2026-06-16 08:45:00 +0800

    update

--- android/app/src/main/java/com/rivan/gcash/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.rivan.gcash;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- start_server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting local web server on port 8000...
-echo Phone QR code will point to: http://192.168.1.16:8000/www/index.html
-python -m http.server 8000
-pause
